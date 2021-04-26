5bcdbbff6d2f298b87c41c1f7e00d6501111a0bd
Dana Musilova <dana.musilova@email.cz>
2021-04-26 20:26:01 +0200

    Komponenta color

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('funguju');
+
+const Color = (props) => {
+  return `
+    <div class="color-option__box"></div>
+    <p class="color-option__name">${props}</p>
+  `;
+};
+
+const colorElm = document.querySelector('.color-option');
+colorElm.innerHTML = Color('bílá');
+colorElm.innerHTML += Color('černá');
+colorElm.innerHTML += Color('červená');
+colorElm.innerHTML += Color('nebesky modrá');
+
+const colorBox = document.querySelector('.color-option__box');
+colorBox.style.Color = 
